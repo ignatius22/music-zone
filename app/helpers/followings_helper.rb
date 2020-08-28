a6305da27581af854c815cfd202156e2ce9294bd
Ignatius22 <igee54077@gmail.com>
2020-08-28 17:03:03 +0100

    add test for user model

--- app/helpers/followings_helper.rb
+++ app/helpers/followings_helper.rb
@@ -1,2 +1,16 @@
 module FollowingsHelper
+
+	def render_follows(user)
+		  unless current_user.id == @user.id 
+            if current_user.followed.any? { |person| person.followedId == @user.id }
+                link_to( unfollow_path(@user), method: :delete,class: "font-unfollow-icon" ) do
+                  link_to icon('minus-circle', id: 'my-icon')
+                end
+              else
+                 link_to(follow_path(@user), class: "font-follow-icon" ) do
+                  link_to icon('plus-circle', id: 'my-icon')
+                end 
+               end 
+          end 
+	end
 end
