850546e60f481a2666ccbb07b9eeb86a521c2930
Ignatius22 <igee54077@gmail.com>
2020-08-29 10:52:54 +0100

    fix more linter error

--- app/helpers/followings_helper.rb
+++ app/helpers/followings_helper.rb
@@ -1,14 +1,13 @@
 module FollowingsHelper
-  def render_follows(user)
-    unless current_user.id == @user.id
-      if current_user.followed.any? { |person| person.followedId == @user.id }
-        link_to(unfollow_path(@user), method: :delete, class: "font-unfollow-icon") do
-          link_to icon('minus-circle', id: 'my-icon')
-        end
-      else
-        link_to(follow_path(@user), class: "font-follow-icon") do
-          link_to icon('plus-circle', id: 'my-icon')
-        end
+  def render_follows(_user)
+    (return if current_user.id == @user.id)
+    if current_user.followed.any? { |person| person.followedId == @user.id }
+      link_to(unfollow_path(@user), method: :delete, class: 'font-unfollow-icon') do
+        link_to icon('minus-circle', id: 'my-icon')
+      end
+    else
+      link_to(follow_path(@user), class: 'font-follow-icon') do
+        link_to icon('plus-circle', id: 'my-icon')
       end
     end
   end
