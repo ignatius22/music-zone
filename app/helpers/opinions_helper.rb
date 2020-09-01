a02c0db831ee632d04b22450800874a258da2dd9
Ignatius22 <igee54077@gmail.com>
2020-09-01 09:28:01 +0100

    add helper for opinion

--- app/helpers/opinions_helper.rb
+++ app/helpers/opinions_helper.rb
@@ -1,2 +1,9 @@
 module OpinionsHelper
+	def pluralized_name_for(opinion)
+  if opinion.Author[:Photo].nil?
+    gravatar_for(opinion.Author, size: 50)
+  else
+    image_tag(opinion.Author.Photo.thumb.url)
+  end
+end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+ def user_render(users)
+	users.each do |user|
+	content_tag(:div, class:"who-follow-profile")
+	content_tag(:div,class:"picture")
+	    link_to user_path(user), class: 'user' do 
+	    unless user[:Photo].nil?
+	    image_tag(user.Photo.thumb.url, class: 'photo')
+	      else
+	     gravatar_for(user, size: 50)
+	      end
+	     content_tag(:h6, user.Fullname)
+	    end
+	end
+	end
 end
